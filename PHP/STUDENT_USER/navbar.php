737dad72d6b4d005ddfee0c441480eca374bc238
Bjakes <bnjeakor@gmail.com>
2025-03-01 18:29:28 +0000

    Create navbar.php

---
+++ PHP/STUDENT_USER/navbar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>      
+    <title>
+        
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, inital-scale=1">
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand">GOSHEN ONLINE LIBRARY MANAGEMENT SYSTEM</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">HOME-PAGE</a></li>
+                <li><a href="books.php">BOOKS</a></li>
+                <li><a href="feedback.php">FEEDBACK</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right ">                
+                <li><a href="student_login.php"><span  class="glyhicon glyphicon-log-in"> LOGIN</span></a></li>
+                <li><a href="student_login.php"><span  class="glyhicon glyphicon-log-out"> LOGIN OUT</span></a></li>
+                <li><a href="registration.php"><span  class="glyhicon glyphicon-user"> SIGN UP</span></a></li>
+                <li><a href="">ADMIN_LOGIN</a></li>
+
+
+            </ul>
+        </div>
+
+    </nav>
+    <!--
+    <header style="height: 100px;">
+        <div class="logo">           
+            <h1 style="color: white; font-size: 20px; line-height: 20px; margin-top: 20px;">GOSHEN ONLINE LIBRARY MANAGEMENT SYSTEM</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html">HOME-PAGE</a></li>                
+                <li><a href="student_login.html">STUDENT-LOGIN</a></li>
+                <li><a href="registration.html">REGISTRATION</a></li>
+                <li><a href="books.html">BOOKS</a></li>
+
+            </ul>
+        </nav>
+ 
+    </header>
+    -->
+
+
+
+
+</body>
+</html>
